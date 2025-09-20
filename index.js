b71451bc81e0396570e58603ede38651b4540288
divya <divyasoniworld@gmail.com>
2025-09-20 03:44:54 -0700

    fix issue

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// server.js
 const express = require("express");
 const http = require("http");
 const { Server } = require("socket.io");
@@ -6,49 +7,32 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, { cors: { origin: "*" } });
 
-let users = {}; // { number: socketId }
+let users = {};
 
 io.on("connection", (socket) => {
-  console.log("Connected:", socket.id);
-
   socket.on("register", (number) => {
     users[number] = socket.id;
-    socket.number = number;
-    io.emit("users", Object.keys(users));
+    io.emit("updateUsers", users);
   });
 
-  socket.on("call-user", ({ from, to }) => {
-    if (users[to]) {
-      io.to(users[to]).emit("incoming-call", { from });
-    } else {
-      io.to(users[from]).emit("user-unavailable", { to });
-    }
-  });
-
-  socket.on("call-response", ({ from, to, accepted }) => {
-    if (users[to]) {
-      io.to(users[to]).emit("call-response", { from, accepted });
-    }
-  });
-
-  socket.on("offer", ({ to, offer }) => {
-    if (users[to]) io.to(users[to]).emit("offer", { offer, from: socket.number });
+  socket.on("offer", ({ to, from, offer }) => {
+    io.to(users[to]).emit("offer", { from, offer });
   });
 
   socket.on("answer", ({ to, answer }) => {
-    if (users[to]) io.to(users[to]).emit("answer", { answer, from: socket.number });
+    io.to(users[to]).emit("answer", { answer });
   });
 
   socket.on("ice-candidate", ({ to, candidate }) => {
-    if (users[to]) io.to(users[to]).emit("ice-candidate", { candidate });
+    io.to(users[to]).emit("ice-candidate", { candidate });
   });
 
   socket.on("disconnect", () => {
-    if (socket.number) {
-      delete users[socket.number];
-      io.emit("users", Object.keys(users));
+    for (let num in users) {
+      if (users[num] === socket.id) delete users[num];
     }
+    io.emit("updateUsers", users);
   });
 });
 
-server.listen(5000, () => console.log("Server running on :5000"));
+server.listen(5000, () => console.log("Server running on 5000"));
