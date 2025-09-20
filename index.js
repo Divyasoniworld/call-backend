c2236505c1cf489d852a77009b907a459a021baa
divya <divyasoniworld@gmail.com>
2025-09-20 08:16:17 -0700

    fix issue

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-// server.js (backend)
+// server.js
+
 const express = require("express");
 const http = require("http");
 const { Server } = require("socket.io");
@@ -7,19 +8,24 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, { cors: { origin: "*" } });
 
-let users = {};
+let users = {}; // Stores { 'phoneNumber': 'socketId' }
 
 io.on("connection", (socket) => {
+  console.log("A user connected:", socket.id);
+
   socket.on("register", (number) => {
+    console.log(`Registering ${number} to ${socket.id}`);
     users[number] = socket.id;
-    io.emit("updateUsers", users);
+    io.emit("updateUsers", Object.keys(users)); // Send only online numbers
   });
 
   socket.on("offer", ({ to, from, offer }) => {
+    console.log(`Offer from ${from} to ${to}`);
     io.to(users[to]).emit("offer", { from, offer });
   });
 
   socket.on("answer", ({ to, answer }) => {
+    console.log(`Answer to ${to}`);
     io.to(users[to]).emit("answer", { answer });
   });
 
@@ -42,13 +48,14 @@ io.on("connection", (socket) => {
   });
 
   socket.on("disconnect", () => {
+    console.log("User disconnected:", socket.id);
     for (let num in users) {
       if (users[num] === socket.id) {
         delete users[num];
       }
     }
-    io.emit("updateUsers", users);
+    io.emit("updateUsers", Object.keys(users));
   });
 });
 
-server.listen(5000, () => console.log("Server running on 5000"));
+server.listen(5000, () => console.log("Server running on port 5000"));
